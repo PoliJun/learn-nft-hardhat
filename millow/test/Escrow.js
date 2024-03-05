c1ce6280f17fe15f7610457834756b630e42a7b1
PoliJun <rivenloving@gmail.com>
2024-03-06 05:06:09 +0800

    test :saves the address

--- millow/test/Escrow.js
+++ millow/test/Escrow.js
@@ -7,12 +7,43 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-let realEstate;
-
 describe("Escrow", () => {
+    let realEstate, escrow;
+    let buyer, seller, inspector, lender;
+
+    it("Returns NFT address", async () => {});
+
+    it("Returns seller", async () => {});
+
+    it("Returns inspector", async () => {});
+
+    it("Returns lender", async () => {});
+
     it("saves the address", async () => {
+        // Setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+        // Deploy Real Estate Contract
         const RealEstate = await ethers.getContractFactory("RealEstate");
         realEstate = await RealEstate.deploy();
         console.log(realEstate.address);
+        // Mint
+        let transaction = await realEstate
+            .connect(seller)
+            .mint("../metadata/1.json");
+        await transaction.wait();
+        const Escrow = await ethers.getContractFactory("Escrow");
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address,
+        );
+
+        let result = await escrow.nftAddress();
+        expect(result).to.equal(realEstate.address);
+
+        result = await escrow.seller();
+        expect(result).to.equal(seller.address);
     });
 });
