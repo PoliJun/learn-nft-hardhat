ca7929fc167bda7c2e812311b4a0fd993c3315fb
PoliJun <rivenloving@gmail.com>
2024-03-06 01:13:41 +0800

    Update project configuration and code. hardhat test

--- millow/test/Escrow.js
+++ millow/test/Escrow.js
@@ -1,8 +1,18 @@
+// test
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { it, describe } = require("mocha");
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("Escrow", () => {});
+let realEstate;
+
+describe("Escrow", () => {
+    it("saves the address", async () => {
+        const RealEstate = await ethers.getContractFactory("RealEstate");
+        realEstate = await RealEstate.deploy();
+        console.log(realEstate.address);
+    });
+});
